ded9973ef34701b4d04a6e23ba631f537fc2ceba
progfay <shun1856shun@gmail.com>
2020-06-26 23:09:39 +0900

    refactor: make class fields to global consistants

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const ROWS = 30
+const ROWS_MAG = 10 // > 1.0
+const MARGINS = 10 // < ROWS
+const MARGINS_MAG = 3 // > 1.0
+const LINE_HEIGHT = 1.2 // rem (default value)
+
 const LINE_COUNTS = 10000
 const padLength = Math.log10(LINE_COUNTS) + 1
 const text = new Array(LINE_COUNTS).fill('').map((_, i) => i.toString().padStart(padLength, '0')).join('\n')
@@ -22,14 +28,11 @@ const lastIndexOf = (text, target, count) => {
 }
 
 class TextareaVirtualized extends HTMLElement {
-  ROWS = 30
-  ROWS_MAG = 10 // > 1.0
-  MARGINS = 10 // < ROWS
-  MARGINS_MAG = 3 // > 1.0
-  LINE_HEIGHT = 1.2 // rem (default or callback)
 
   constructor () {
     super()
+
+    this.lineHeight = LINE_HEIGHT
 
     this.shadow = this.attachShadow({mode: 'closed'})
     const style = document.createElement('style')
@@ -46,7 +49,7 @@ class TextareaVirtualized extends HTMLElement {
         overflow-x: hidden;
         overflow-y: scroll;
         border: solid 1px black;
-        height: ${this.ROWS * this.LINE_HEIGHT}rem;
+        height: ${ROWS * this.lineHeight}rem;
       }
 
       #container::-webkit-scrollbar {
@@ -66,12 +69,12 @@ class TextareaVirtualized extends HTMLElement {
       }
 
       #upper {
-        top: calc(100% * ${this.MARGINS} / ${this.ROWS});
+        top: calc(100% * ${MARGINS} / ${ROWS});
         background-color: rgb(255, 0, 0);
       }
 
       #lower {
-        bottom: calc(100% * ${this.MARGINS} / ${this.ROWS});
+        bottom: calc(100% * ${MARGINS} / ${ROWS});
         background-color: rgb(0, 0, 255);
       }
     `
@@ -87,7 +90,7 @@ class TextareaVirtualized extends HTMLElement {
 
     this.textarea = document.createElement('textarea')
     this.textarea.setAttribute('id', 'textarea')
-    this.textarea.setAttribute('rows', this.ROWS * this.ROWS_MAG)
+    this.textarea.setAttribute('rows', ROWS * ROWS_MAG)
     this.container.appendChild(this.textarea)
 
     this.lower = document.createElement('div')
@@ -97,7 +100,7 @@ class TextareaVirtualized extends HTMLElement {
     // const text = this.value
     // const text = text // (use global variable)
 
-    const index = indexOf(text, '\n', this.ROWS * this.ROWS_MAG)
+    const index = indexOf(text, '\n', ROWS * ROWS_MAG)
     this.upperVirtualizedText = ''
     this.textarea.value = text.substring(0, index)
     this.lowerVirtualizedText = text.substring(index)
@@ -115,14 +118,14 @@ class TextareaVirtualized extends HTMLElement {
     for (const entry of entries) {
       if (!entry.isIntersecting) return
       if (this.upperVirtualizedText === '') return
-      const upperIndex = lastIndexOf(this.upperVirtualizedText, '\n', this.MARGINS * this.MARGINS_MAG)
+      const upperIndex = lastIndexOf(this.upperVirtualizedText, '\n', MARGINS * MARGINS_MAG)
       const text = (upperIndex !== -1 ? this.upperVirtualizedText.substring(upperIndex) : this.upperVirtualizedText) + this.textarea.value
       this.upperVirtualizedText = upperIndex !== -1 ? this.upperVirtualizedText.substring(0, upperIndex) : ''
-      const index = indexOf(text, '\n', this.ROWS * this.ROWS_MAG)
+      const index = indexOf(text, '\n', ROWS * ROWS_MAG)
       this.textarea.value = text.substring(0, index)
       this.lowerVirtualizedText = text.substring(index) + this.lowerVirtualizedText
-      this.container.scrollBy(0, this.LINE_HEIGHT * this.MARGINS * this.MARGINS_MAG)
-      this.textarea.scrollBy(0, -1 * this.LINE_HEIGHT)
+      this.container.scrollBy(0, this.lineHeight * MARGINS * MARGINS_MAG)
+      this.textarea.scrollBy(0, -1 * this.lineHeight)
       this.textarea.selectionStart = Math.min(Math.max(this.selectionStart - this.upperVirtualizedText.length, 0), this.textarea.value.length)
       this.textarea.selectionEnd = Math.min(Math.max(this.selectionEnd - this.upperVirtualizedText.length, 0), this.textarea.value.length)
       return
@@ -133,14 +136,14 @@ class TextareaVirtualized extends HTMLElement {
     for (const entry of entries) {
       if (!entry.isIntersecting) return
       if (this.lowerVirtualizedText === '') return
-      const lowerIndex = indexOf(this.lowerVirtualizedText, '\n', this.MARGINS * this.MARGINS_MAG)
+      const lowerIndex = indexOf(this.lowerVirtualizedText, '\n', MARGINS * MARGINS_MAG)
       const text = this.textarea.value + (lowerIndex !== -1 ? this.lowerVirtualizedText.substring(0, lowerIndex) : this.lowerVirtualizedText)
       this.lowerVirtualizedText = lowerIndex !== -1 ? this.lowerVirtualizedText.substring(lowerIndex) : ''
-      const index = lastIndexOf(text, '\n', this.ROWS * this.ROWS_MAG)
+      const index = lastIndexOf(text, '\n', ROWS * ROWS_MAG)
       this.textarea.value = text.substring(index)
       this.upperVirtualizedText = this.upperVirtualizedText + text.substring(0 ,index)
-      this.container.scrollBy(0, -1 * this.LINE_HEIGHT * (this.MARGINS * this.MARGINS_MAG - 1))
-      this.textarea.scrollBy(0, this.LINE_HEIGHT)
+      this.container.scrollBy(0, -1 * this.lineHeight * (MARGINS * MARGINS_MAG - 1))
+      this.textarea.scrollBy(0, this.lineHeight)
       this.textarea.selectionStart = Math.min(Math.max(this.selectionStart - this.upperVirtualizedText.length, 0), this.textarea.value.length)
       this.textarea.selectionEnd = Math.min(Math.max(this.selectionEnd - this.upperVirtualizedText.length, 0), this.textarea.value.length)
       return
@@ -164,7 +167,7 @@ class TextareaVirtualized extends HTMLElement {
             ? this.upperVirtualizedText.length + this.textarea.selectionEnd
             : 0
           const text = this.upperVirtualizedText + this.textarea.value + this.lowerVirtualizedText
-          const index = indexOf(text, '\n', this.ROWS * this.ROWS_MAG)
+          const index = indexOf(text, '\n', ROWS * ROWS_MAG)
           this.textarea.value = index !== -1 ? text.substring(0, index) : text
           this.upperVirtualizedText = ''
           this.lowerVirtualizedText = index !== -1 ? text.substring(index) : ''
@@ -184,12 +187,12 @@ class TextareaVirtualized extends HTMLElement {
             : this.upperVirtualizedText.length + this.textarea.value.length + this.lowerVirtualizedText.length
           this.selectionEnd = this.upperVirtualizedText.length + this.textarea.value.length + this.lowerVirtualizedText.length
           const text = this.upperVirtualizedText + this.textarea.value + this.lowerVirtualizedText
-          const index = lastIndexOf(text, '\n', this.ROWS * this.ROWS_MAG)
+          const index = lastIndexOf(text, '\n', ROWS * ROWS_MAG)
           this.textarea.value = index !== -1 ? text.substring(index) : text
           this.upperVirtualizedText = index !== -1 ? text.substring(0, index) : ''
           this.lowerVirtualizedText = ''
-          this.container.scrollTo(0, this.LINE_HEIGHT * (this.ROWS * this.ROWS_MAG))
-          this.textarea.scrollBy(0, this.LINE_HEIGHT)
+          this.container.scrollTo(0, this.lineHeight * (ROWS * ROWS_MAG))
+          this.textarea.scrollBy(0, this.lineHeight)
           this.textarea.selectionStart = event.shiftKey
             ? Math.max(this.selectionStart - this.upperVirtualizedText.length, 0)
             : this.textarea.value.length
@@ -203,8 +206,8 @@ class TextareaVirtualized extends HTMLElement {
   }
 
   connectedCallback() {
-    this.LINE_HEIGHT = this.textarea.getBoundingClientRect().height / (this.ROWS * this.ROWS_MAG)
-    this.container.style.setProperty('height', `${this.ROWS * this.LINE_HEIGHT}px`)
+    this.lineHeight = this.textarea.getBoundingClientRect().height / (ROWS * ROWS_MAG)
+    this.container.style.setProperty('height', `${ROWS * this.lineHeight}px`)
     this.upperTextareaIntersectionObserver.observe(this.upper)
     this.lowerTextareaIntersectionObserver.observe(this.lower)
   }
